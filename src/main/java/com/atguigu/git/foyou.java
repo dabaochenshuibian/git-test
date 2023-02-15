a400beb6566db9a097f7eb9581b6cbc1c6d89740
touchin <9305213+vvvvvcxnn@user.noreply.gitee.com>
2023-02-15 19:54:37 +0800

    my second commit

--- src/main/java/com/atguigu/git/foyou.java
+++ src/main/java/com/atguigu/git/foyou.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class foyou {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
+        System.out.println("Hello,Git2");
     }
 }
