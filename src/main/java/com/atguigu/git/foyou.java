5d338fbe849633990c346fdfa927f952d562986c
touchin <9305213+vvvvvcxnn@user.noreply.gitee.com>
2023-02-15 20:11:05 +0800

    master commit

--- src/main/java/com/atguigu/git/foyou.java
+++ src/main/java/com/atguigu/git/foyou.java
@@ -6,5 +6,6 @@ public class foyou {
         System.out.println("Hello,Git2");
         System.out.println("Hello,Git3");
         System.out.println("Hello,Git4");
+        System.out.println("Hello,Git4");
     }
 }
